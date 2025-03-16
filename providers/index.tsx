21aec3472aab1332d9b2b42270c80ced78122521
lou1s <lou1svuong.dev@gmail.com>
2025-03-16 21:49:03 +0700

    add Providers component to wrap children with ThemeProvider for consistent theming

---
+++ providers/index.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "./theme-provider";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
