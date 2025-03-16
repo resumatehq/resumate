b65c0b81d63d5c84039064160110212ff7dc3a14
lou1s <lou1svuong.dev@gmail.com>
2025-03-17 00:54:04 +0700

    add new sections to Home component for enhanced content display

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,24 @@ import { HomeHeader } from "@/components/layouts/headers/home-headers";
 import LogoCloudSection from "@/containers/home/logo-cloud-section";
 import FeaturesSection from "@/containers/home/features-section";
 import FooterSection from "@/components/layouts/footers/footer";
+import StatsSection from "@/containers/home/stats-section";
+import CanvasCursor from "@/components/canvas-cursor";
+import { StepsSection } from "@/containers/home/steps-section";
+import FaqsSection from "@/containers/home/faqs-section";
+import TestimonialsSection from "@/containers/home/testimonials-section";
 
 export default function Home() {
   return (
     <>
+    <CanvasCursor />
       <HomeHeader />
       <HeroSection />
       <LogoCloudSection />
+      <StatsSection/>
       <FeaturesSection />
+      <StepsSection />
+      <FaqsSection />
+      <TestimonialsSection />
       <FooterSection />
     </>
   );
