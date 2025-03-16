4b4cbc36ff4c60f0346f0ad5cccfcb9c64fc6c97
lou1s <lou1svuong.dev@gmail.com>
2025-03-17 00:58:21 +0700

    change default theme from dark to light in Providers component

--- providers/index.tsx
+++ providers/index.tsx
@@ -4,7 +4,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="dark"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
     >
