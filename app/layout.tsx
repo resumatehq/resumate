7706f83cd691479cb8bc41515ae836c6dddcc18e
lou1s <lou1svuong.dev@gmail.com>
2025-03-17 07:50:14 +0700

    add custom local font PingFangSC to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,37 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+
 import "./globals.css";
 import { ResumeProvider } from "@/context/resume-context";
 import { Providers } from "@/providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({
+  src: [
+    {
+      path: "./fonts/PingFangSC-Thin.woff2",
+      weight: "100",
+    },
+    {
+      path: "./fonts/PingFangSC-Light.woff2",
+      weight: "300",
+    },
+    {
+      path: "./fonts/PingFangSC-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "./fonts/PingFangSC-Regular.woff2",
+      weight: "400",
+    },
+    {
+      path: "./fonts/PingFangSC-Semibold.woff2",
+      weight: "600",
+    },
+  ],
+  display: "swap",
+  variable: "--font-PingFangSC",
+});
 
 export const metadata: Metadata = {
   title: "ResumeAI - Professional Resume Builder",
@@ -18,8 +44,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
+      <body className={`${myFont.className} ${myFont.variable}`}>
         <Providers>
           <ResumeProvider>{children}</ResumeProvider>
         </Providers>
