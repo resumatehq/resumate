3b926cecb0a27ac259243052a874963bb4c291fe
lou1s <lou1svuong.dev@gmail.com>
2025-03-17 22:23:01 +0700

    add LoginForm and LoginPage components for user authentication

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import LoginForm from "./login-form";
+
+export default function LoginPage() {
+  return (
+    <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+      <LoginForm />
+    </section>
+  );
+}
